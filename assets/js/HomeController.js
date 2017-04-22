fd661640001e35487174c911812650287fa2dc5f
Sendil Kumar <smiledigital2004@gmail.com>
2017-04-22 12:02:01 +0530

    completed percentage indicators

--- assets/js/HomeController.js
+++ assets/js/HomeController.js
@@ -6,6 +6,9 @@
     function HomeController(UserService, $rootScope, $http, $filter) {
         var vm = this;
         vm.serachPlanet = serachPlanet;
+        vm.actualPop = actualPop;
+        //vm.sortNumber = sortNumber;
+        vm.population = [];
         vm.user = null;
         vm.allUsers = [];
         vm.planetsDetails = [];
@@ -17,6 +20,13 @@
             $http.get('http://swapi.co/api/planets/').then(function (response) {
                 //console.log('planets',response);
                 vm.planetsDetails = response.data.results;
+                angular.forEach(vm.planetsDetails, function (_pla) {
+                    if (_pla.population != 'unknown') {
+                        vm.population.push(parseInt(_pla.population));
+                    }
+                })
+                vm.population.sort(sortNumber);
+                // console.log(vm.population.max());
             }, function myError(response) {});
             loadCurrentUser();
             //loadAllUsers();
@@ -30,12 +40,33 @@
 
         function serachPlanet() {
             //console.log('seraching..');
-            if (vm.planetsDetails.length) {
+            if (vm.planetsDetails.length && vm.searchIn != '') {
                 vm.searchResult = $filter('filter')(vm.planetsDetails, vm.searchIn);
-                console.log(vm.searchResult);
-            }else{
-                vm.searchResult = [];
+                //console.log(vm.searchResult);
             }
+            else {
+                vm.searchResult = null;
+            }
+        }
+
+        function sortNumber(a, b) {
+            return a - b;
+        }
+        Array.prototype.max = function () {
+            return Math.max.apply(null, this);
+        };
+
+        function actualPop(val) {
+            //console.log(val);
+            var percent = '';
+            if (val != 'unknown') {
+                 percent = parseInt(val)/vm.population.max() * 10 +'em';
+
+            }else{
+                percent = 0+'em';
+            }
+            console.log('percent', percent);
+            return percent;
         }
         /*function loadAllUsers() {
             UserService.GetAll().then(function (users) {
