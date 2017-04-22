2587ecebc371af566c50215dab1964de6bfae310
Sendil Kumar <smiledigital2004@gmail.com>
2017-04-22 11:12:05 +0530

    Search is implemented

--- assets/js/HomeController.js
+++ assets/js/HomeController.js
@@ -1,19 +1,25 @@
 (function () {
     'use strict';
     angular.module('app').controller('HomeController', HomeController);
-    HomeController.$inject = ['UserService', '$rootScope'];
+    HomeController.$inject = ['UserService', '$rootScope', '$http', '$filter'];
 
-    function HomeController(UserService, $rootScope) {
+    function HomeController(UserService, $rootScope, $http, $filter) {
         var vm = this;
+        vm.serachPlanet = serachPlanet;
         vm.user = null;
         vm.allUsers = [];
+        vm.planetsDetails = [];
         $rootScope.currentUser.userLogged = true;
         //vm.deleteUser = deleteUser;
         initController();
 
         function initController() {
+            $http.get('http://swapi.co/api/planets/').then(function (response) {
+                //console.log('planets',response);
+                vm.planetsDetails = response.data.results;
+            }, function myError(response) {});
             loadCurrentUser();
-            loadAllUsers();
+            //loadAllUsers();
         }
 
         function loadCurrentUser() {
@@ -22,12 +28,20 @@
             });
         }
 
-        function loadAllUsers() {
+        function serachPlanet() {
+            //console.log('seraching..');
+            if (vm.planetsDetails.length) {
+                vm.searchResult = $filter('filter')(vm.planetsDetails, vm.searchIn);
+                console.log(vm.searchResult);
+            }else{
+                vm.searchResult = [];
+            }
+        }
+        /*function loadAllUsers() {
             UserService.GetAll().then(function (users) {
                 vm.allUsers = users;
             });
-        }
-
+        }*/
         /*function deleteUser(id) {
             UserService.Delete(id).then(function () {
                 loadAllUsers();
